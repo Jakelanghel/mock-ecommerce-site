b88375560d018604108b3239f976f91b23ba163d
Jake <jacobtlanghel@gmail.com>
2022-04-29 22:43:20 -0400

    init

---
+++ src/Context.js
@@ -0,0 +1,22 @@
+import { getAllByAltText } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
+
+const Context = React.createContext();
+
+const ContextProvider = ({ children }) => {
+  const [photoArr, setPhotoArr] = useState([]);
+  const url =
+    "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPhotoArr(data));
+  }, []);
+
+  console.log(photoArr);
+
+  return <Context.Provider value={{ photoArr }}>{children}</Context.Provider>;
+};
+
+export { ContextProvider, Context };
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">
+        <h2>Pic Some</h2>
+      </Link>
+
+      <Link to="/cart">
+        <img
+          src="./images/cart-shopping-solid.svg"
+          alt="go to shopping cart"
+          className="cart-img"
+        />
+      </Link>
+    </header>
+  );
+}
+
+export default Header;
