f6ada8d07fb60da37a3640f48343b6e2dbd61251
Jake <jacobtlanghel@gmail.com>
2022-04-30 01:23:49 -0400

    add ability to favorite images

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,13 +5,31 @@ const Image = ({ className, img }) => {
   const [hovered, setIsHovered] = useState(false);
   const { toggleFav } = useContext(Context);
 
-  const heartIcon = hovered && (
+  //   const heartIcon = hovered && (
+  //     <img
+  //       src="./images/heart-regular.svg"
+  //       alt="remove from to favorites"
+  //       className="favorite filter-red"
+  //       onClick={() => toggleFav(img.id)}
+  //     />
+  //   );
+
+  const heartIcon = img.isFavorite ? (
     <img
       src="./images/heart-solid.svg"
-      alt="add to favorites"
+      alt="remove from to favorites"
       className="favorite filter-red"
       onClick={() => toggleFav(img.id)}
     />
+  ) : (
+    hovered && (
+      <img
+        src="./images/heart-regular.svg"
+        alt="remove from to favorites"
+        className="favorite filter-red"
+        onClick={() => toggleFav(img.id)}
+      />
+    )
   );
 
   const plusIcon = hovered && (
